4b8a076e94166b202774e39ee54f4c21ff09c926
HariHarani Theivendram <145446952+htheivendram@users.noreply.github.com>
2024-11-18 21:44:36 +0100

    Delete Portfolio BTS directory

--- Portfolio BTS/test/script.js
+++
@@ -1,8 +0,0 @@
-// Sélectionner les éléments du DOM
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-// Ajouter un événement au clic
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
